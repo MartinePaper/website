cbee451cfe4c040e154e698dd0d564e36c7a3417
Elizabeth Hargreaves <liz.hargreaves@gmail.com>
2012-10-04 20:02:50 -0500

    Gallery & detail redesign
    - Merged Gallery & Collections
    - Laid out vertical and horizontal detail views

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,22 @@
-console.log('This would be the main JS file.');
+
+(function() {
+
+	// Initial Setup
+	$(function() {
+		$('.image-thumbnails').find('a').click(function(e) {
+			var anchor = $(e.currentTarget);
+			var detailSize = anchor.attr('data-detail-size');
+			var detailOrientation = anchor.attr('data-detail-orientation');
+			var sectionOrientation = $('#invitation-detail-section').attr('data-section-layout-orientation')
+			var imgSrc = anchor.find('img').attr('src');
+			var detailContainer = $('#invitation-detail-picture')
+			detailContainer.empty();
+			var detailImage = $('<img class="invitation-detail-image" alt="">');
+			detailImage.addClass('invitation-detail-' + sectionOrientation + '-content')
+			detailImage.addClass('invitation-detail-' + detailSize + '-' + detailOrientation);
+			detailImage.attr('src', imgSrc);
+			detailContainer.append(detailImage);
+		});
+	});
+
+})();
